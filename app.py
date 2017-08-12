70c8fd7a13f1fe0a3e217205dedc473d9cd75820
Simon Egersand <s.egersand@gmail.com>
2017-08-12 02:24:13 +0200

    Add simple error handling when no horoscopes

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ import emailer
 import requester
 import templater
 
-horoscopes = requester.get_horoscopes()
-html_email = templater.build(horoscopes)
 
-emailer.send('Dagens Horoskop', html_email)
+def run():
+    horoscopes = None
+    try:
+        horoscopes = requester.get_horoscopes()
+    except LookupError:
+        print "Aborting since there's no horoscopes to process"
+        quit()
+
+    html_email = templater.build(horoscopes)
+
+    emailer.send('Dagens Horoskop', html_email)
+
+
+if __name__ == "__main__":
+    run()
--- requester.py
+++ requester.py
@@ -8,11 +8,18 @@ from Horoscope import Horoscope
 
 
 def get_horoscopes():
+    # Get config where are the websites + xpaths are stored
     paths = config.horoscopes
 
     result = []
     for hs in paths:
-        result.append(_set_horoscope_result(hs))
+        try:
+            result.append(_set_horoscope_result(hs))
+        except LookupError:
+            pass
+
+    if len(result) == 0:
+        raise LookupError
 
     return result
 
@@ -20,5 +27,10 @@ def get_horoscopes():
 def _set_horoscope_result(hs):
     page = requests.get(hs.get('website_url'))
     tree = html.fromstring(page.content)
-    result = tree.xpath(hs.get('xpath'))[0].text
+    # Let's see if this "generic" way works with all websites
+    result = tree.xpath(hs.get('xpath'))[0].text.strip()
+
+    if len(result) == 0:
+        raise LookupError
+
     return Horoscope(hs.get('website_name'), hs.get('website_url'), result)
