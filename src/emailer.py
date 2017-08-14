d9619e398c87b0837dac2b9862af5af3b2579570
Simon Egersand <s.egersand@gmail.com>
2017-08-14 23:11:51 +0200

    Do not send email during development

--- src/emailer.py
+++ src/emailer.py
@@ -2,16 +2,21 @@
 
 import yagmail
 import logging
+import config_handler as cfgh
 
 logger = logging.getLogger(__name__)
 
 
 def send(password, username, sender_alias, recipients, subject, content):
-    logger.info('Will send e-mail to ' + str(recipients))
-    yag = yagmail.SMTP({username: sender_alias}, password)
-    send_result = yag.send(recipients, subject, content)
+    if cfgh.is_production:
+        logger.info('Will send e-mail to %s', str(recipients))
+        yag = yagmail.SMTP({username: sender_alias}, password)
+        send_result = yag.send(recipients, subject, content)
 
-    if send_result is False:
-        logger.info('Was unable to send email, not sure why')
+        # False means it did not send, everything else is OK
+        if send_result is False:
+            logger.info('Was unable to send email, not sure why')
+        else:
+            logger.info('Email successfully sent')
     else:
-        logger.info('Email successfully sent')
+        logger.info('[noop] Will send e-mail to %s', str(recipients))
