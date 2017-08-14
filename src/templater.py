79679dddf28f1d319052c20d31fe42cb2cb789d2
Simon Egersand <s.egersand@gmail.com>
2017-08-14 21:30:16 +0200

    Refactor template for clarity

--- src/templater.py
+++ src/templater.py
@@ -4,16 +4,24 @@
 import logging
 
 logger = logging.getLogger(__name__)
+
 HEADER = 'Dagens horoskop för stjärntecknet Vattumanen'
 
 
 def build(horoscopes):
-    logger.info('Building e-mail template')
+    logger.info('Start building horoscopes template')
+    template = _build_header(HEADER)
+    template = _build_horoscopes(template, horoscopes)
+
+    logger.info('Completed building horoscopes template')
+    return template
+
+
+def _build_horoscopes(template, horoscopes):
     i = 0
     size = len(horoscopes)
-    template = _format_header(HEADER)
     for hs in horoscopes:
-        template += _format_line(
+        template += _build_hs_line(
             hs.get('website_name'),
             hs.get('website_url'),
             hs.get('result')
@@ -23,15 +31,13 @@ def build(horoscopes):
         if i != (size - 1):
             template += '<hr/>'
         i += 1
-
-    logger.info('Completed building e-mail template')
     return template
 
 
-def _format_header(header):
+def _build_header(header):
     return '<h2>' + header + '</h2>\n'.encode('utf-8')
 
 
-def _format_line(name, website, horoscope):
+def _build_hs_line(name, website, horoscope):
     return '<h3><a href="' + website + '">' + name + '</a></h3><p>' + horoscope + '</p>\n' \
         .encode('utf-8')
