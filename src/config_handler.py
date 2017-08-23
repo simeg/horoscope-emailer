fdc7c2aa45639f6d58a9dae6fbba0e72a5300268
Simon Egersand <s.egersand@gmail.com>
2017-08-24 00:25:13 +0200

    Put configs in separate config folder

--- src/config_handler.py
+++ src/config_handler.py
@@ -12,11 +12,11 @@ def get(key):
 
 
 def default_config():
-    return _get_file('src/config.yaml')
+    return _get_file('src/config/config.yaml')
 
 
 def _private_config():
-    return _get_file('src/private_config.yaml')
+    return _get_file('src/config/private_config.yaml')
 
 
 def _get_file(path):
